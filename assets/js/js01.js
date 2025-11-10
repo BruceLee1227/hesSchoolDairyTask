deddbce4a3f08cc707c2df0807fdbe95dea62f88
李岳宏 <120881969+BruceLee1227@users.noreply.github.com>
2025-11-10 14:03:18 +0800

    Day01每日任務

--- assets/js/js01.js
+++ assets/js/js01.js
@@ -1,1 +1,10 @@
 console.log(`Hello from js01.js`);
+let nickName = "李大維";
+let area = "台中市";
+let job = "輪班工程師";
+let hobby = "打電動";
+let jsLearnHour = "3小時";
+
+console.log(
+  `大家好，我是${nickName}，很高興認識各位！我來自${area}，目前是一個${job}，平常的興趣是${hobby}。預計之後每天會花${jsLearnHour}小時學習 JavaScript，未來還請多多指教。`
+);
