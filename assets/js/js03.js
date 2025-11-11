fbc2dea0248439b1af1cfd5d6bceabb747408f9c
李岳宏 <120881969+BruceLee1227@users.noreply.github.com>
2025-11-11 13:51:26 +0800

    Day3

---
+++ assets/js/js03.js
@@ -0,0 +1,9 @@
+console.log(`hello from js03.js`);
+let a = true + true;
+console.log(a, typeof a);
+let b = false + 123;
+console.log(b, typeof b);
+let c = null + 123;
+console.log(c, typeof c);
+let d = null + '123';
+console.log(d, typeof d);
